23b98ee08d630dc9df8f5ba05e6a498d58a51e88
--global <gsyoung247@gmail.com>
2017-10-26 12:45:47 -0700

    style change

--- main.py
+++ main.py
@@ -1,82 +1,94 @@
 # time in minutes unless specified otherwise
+# Structure lights and CO2 as "keep on during time"
+# Structure pumps as "run every X minutes"
+# For reset, hold until pump begins
+
 import time
 import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 #----------------------Constants---------------------------------
-loopDelayInterval 	= 10
+LOOP_DELAY_INTERVAL = 10
 
-intervalFerts 	= 24 * 7 * 60 #fertilizer
-intervalLC	 	= 24 * 60 #liquid carbon
+TIME_LIGHTS 	= 	[600, 1200] #lights (input in minutes)
+TIME_INJECT 	= 	[570, 1140] #co2 in ject (input in minutes)
 
-timeLights 	= 	[600, 1200] #lights (input in minutes)
-timeInject 	= 	[570, 1140] #co2 in ject (input in minutes)
-
-lots = 1e6
+LOTS = 1e6
 # in order of:
 #	Lights 	0
 # 	Inject 	1
-pinListOutput 	= [1, 2] 												##
-pinStatusOutput = [0, 0]
+PINLIST_OUTPUT 	= [1, 2] 												##
 # in order of:
 # 	Ferts
 # 	LC
-pinListPWM 		= [3, 4] 												##
-pinStatusPWM 	= [0, 0]
-dcPWM 			= [50, 50] 												##
-freqPWM 		= [0, 0] 												##
-runtimePWM 		= [5, 1] # in seconds 									##
-intervalPWM 	= [24*7*60, 24*60]
+PINLIST_PWM = [3, 4] 													##
+DC 			= [50, 50] 													##
+RUNTIME 	= [5, 1] # in seconds 										##
+INTERVAL 	= [24*7*60, 24*60]
+PINPWM 		= 18 														##
+FREQ 		= 0		 													##
 # in order of:
 # 	Reset
-pinListInput 	= [5] 													##
+PINLIST_INPUT 	= [5] 													##
 #----------------------------------------------------------------
 #-------------------------Functions------------------------------
-def time_check(timeOn,timeOff,timeCurrent,pinNo):
-	if (timeCurrent > timeOn and timeCurrent < timeOff):
-		if GPIO.output(pinNo, not GPIO.input(pinNo)) == False:
-			print("Pin number",pinNo,
-				"Switched ON at", time.asctime(time.localtime()))
+def is_time(timeOn,timeOff,timeCurrent):
+	return timeCurrent > timeOn and timeCurrent < timeOff
+def is_output(pinNo):
+	return GPIO.output(pinNo, not GPIO.input(pinNo))
+def MDYT():
+	return time.asctime(time.localtime())
+
+def timer_on_off(timeOn,timeOff,timeCurrent,pinNo):
+	"""Timer for lights and CO2 inject"""
+	if is_time(timeOn,timeOff,timeCurrent):
+		if is_output(pinNo) == False:
+			print("Pin number ",pinNo,
+				" switched ON at", MDYT())
 		pinStatus = True	
 	else:
-		if GPIO.output(pinNo, not GPIO.input(pinNo)) == True:
-			print("Pin number",pinNo,
-				"Switched OFF at", time.asctime(time.localtime()))
+		if is_outout(pinNo) == True:
+			print("Pin number ",pinNo,
+				" switched OFF at", MDYT())
 		pinStatus = False
 	return pinStatus
 
-def pump_check(timer,interval,runtime,pwm,dc):
+def pump_on_off(timer,interval,runtime,pinNo,dc,pwm):
+	"""Switch to correct pump and run for set time"""
 	if (timer - interval) > 0:
+		GPIO.output(pinNo, True)
 		pwm.start(dc)
-		print("Pump run on", time.asctime(time.localtime()))
+		print("Pump for ",pinNo," run on", MDYT())
 		time.sleep(runtime)
 		pwm.stop()
+		GPIO.output(pinNo, False)
 		timer = runtime/60
 	else:
 		timer = timer
 	return timer
 #----------------------------------------------------------------
 #----------------------Setup-------------------------------------
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pinListOutput[0], 	GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(pinListOutput[1], 	GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(pinListPWM[0],	 	GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(pinListPWM[1],	 	GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(pinListInput[0], 	GPIO.IN)
+GPIO.setup(PINLIST_OUTPUT[0], 	GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PINLIST_OUTPUT[1], 	GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PINLIST_PWM[0],	 	GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(PINLIST_PWM[1],	 	GPIO.OUT, initial = GPIO.LOW)
 
-fertPWM = GPIO.PWM(pinListPWM[0],freqPWM[0])
-LCPWM 	= GPIO.PWM(pinListPWM[1],freqPWM[1])
-fertPWM.stop()
-LCPWM.stop()
-#----------------------------------------------------------------
+# input with pull-up resistor to 3.3V and 10kOhm
+GPIO.setup(PINLIST_INPUT[0], 	GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
+# pwm
+pwm = GPIO.PWM(PINPWM,FREQ[0])
+pwm.stop()
 
-# Structure lights and CO2 as "keep on during time"
-# Structure pumps as "run every X minutes"
-# For reset, hold until pump begins
-# ---------------------Begin Loop------------------------------------
-# --------Initializing
+# --------Initializing Variables
+pinStatusOutput = [0, 0]
+pinStatusPWM 	= [0, 0]
 timerFerts 		= 0
 timerLC 		= 0
+
 timeAbsPrev 	= time.time()/60
+#----------------------------------------------------------------
+# ---------------------Begin Loop--------------------------------
+
 # --------Loop
 while True:
 	#--------fetch current time
@@ -84,27 +96,29 @@ while True:
 	timeAbs 	= time.time()/60
 	dt 			= timeAbs - timeAbsPrev
 	#--------lights, CO2 check
-	pinStatusOutput[0] = time_check(timeLights[0],timeLights[1],
-									timeCurrent,pinListOutput[0])
-	pinStatusOutput[1] = time_check(timeInject[0],timeLights[1],
-									timeCurrent,pinListOutput[1])
+	pinStatusOutput[0] = timer_on_off(TIME_LIGHTS[0],TIME_LIGHTS[1],
+									timeCurrent,PINLIST_OUTPUT[0])
+	pinStatusOutput[1] = timer_on_off(TIME_INJECT[0],TIME_LIGHTS[1],
+									timeCurrent,PINLIST_OUTPUT[1])
 	# Write to output
-	GPIO.output(pinListOutput,pinStatusOutput)
+	GPIO.output(PINLIST_OUTPUT,pinStatusOutput)
 	#--------Pumps
 	# Ferts
 	timerFerts	+= dt
-	timerFerts 	= pump_check(	timerFerts,intervalPWM[0],
-								runtimePWM[0],fertPWM,dcPWM[0])
+	timerFerts 	= pump_on_off(	timerFerts,INTERVAL[0],
+								RUNTIME[0],PINLIST_PWM[0],
+								DC[0],pwm)
 	# LC
 	timerLC += dt
-	timerLC	= pump_check(	timerLC,intervalPWM[1],
-								runtimePWM[1],LCPWM,dcPWM[1])
+	timerLC	= pump_on_off(	timerLC,INTERVAL[1],
+							RUNTIME[1],PINLIST_PWM[1],
+							DC[1],pwm)
 	# Timer reset
 	if GPIO.input(pinList[4]) == True:
-		timerFerts 	= lots
-		timerLC 	= lots
+		timerFerts 	= LOTS
+		timerLC 	= LOTS
 
 	# end of loop iterations
 	timeAbsPrev = time.time()/60
-	time.sleep(loopDelayInterval)
-#----------------------End Loop-----------------------------------------
+	time.sleep(LOOP_DELAY_INTERVAL)
+#----------------------End Loop-------------------------------------
